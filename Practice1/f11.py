01beda3e2195353fe8c5ee7653fc827f997441cf
Andrew <frayz2507@gmail.com>
2021-02-21 15:13:26 +0300

    Вариант 11 Задания 1.1, 1.2, 1.3

--- Practice1/f11.py
+++ Practice1/f11.py
@@ -5,5 +5,4 @@ def f11(x):
     d1 = ((x ** 2) + 45 * (x ** 3)) / (math.exp(x) + math.exp(x) + 90)
     d2 = ((30 * (x ** 7) + math.tan(x)) / (27 * (x ** 7) - math.fabs(x)))
     a = math.sqrt(10 * (x ** 5) - math.cos(x))
-    res = d1 - d2 + a
-    return '%.2e' % res
+    return d1 - d2 + a
--- Practice1/f12.py
+++ Practice1/f12.py
@@ -3,17 +3,12 @@ import math
 
 def f12(x):
     if x < 68:
-        res = x + 5 * (x ** 4)
-        return '%.2e' % res
+        return x + 5 * (x ** 4)
     elif x < 93:
-        res = ((x ** 5) + 9 * (x ** 8))
-        return '%.2e' % res
+        return (x ** 5) + 9 * (x ** 8)
     elif x < 122:
-        res = (((50 * (x ** 3)) - math.cos(x)) ** 8) - 23 * (x ** 6)
-        return '%.2e' % res
+        return (((50 * (x ** 3)) - math.cos(x)) ** 8) - 23 * (x ** 6)
     elif x < 154:
-        res = 99 * ((math.tan(x) + math.tan(x) + 35) ** 6) + math.log(x)
-        return '%.2e' % res
+        return 99 * ((math.tan(x) + math.tan(x) + 35) ** 6) + math.log(x)
     else:
-        res = (math.cos(x ** 6) - (x ** 8))
-        return '%.2e' % res
+        return math.cos(x ** 6) - (x ** 8)
