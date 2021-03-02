b03df8b7e366485673e1eae8051817d16e7834bb
Andrew <frayz2507@gmail.com>
2021-03-02 12:27:38 +0300

    Вариант 11 Задания 2.1, 2.2

--- Practice2/f21.py
+++ Practice2/f21.py
@@ -3,30 +3,27 @@ def f21(x):
         return 10
     elif x[4] == 'bison':
         return 9
-    else:
-        if x[4] == 'cson':
-            if x[0] == 2003:
+    elif x[4] == 'cson':
+        if x[0] == 2003:
+            if x[3] == 2016:
+                if x[2] == 'vala':
+                    return 0
+                elif x[2] == 'ada':
+                    return 1
+                elif x[2] == 'red':
+                    return 2
+            elif x[3] == 1982:
+                if x[1] == 'xbase':
+                    return 3
+                elif x[1] == 'gn':
+                    return 4
+        elif x[0] == 1960:
+            if x[1] == 'gn':
                 if x[3] == 2016:
-                    if x[2] == 'vala':
-                        return 0
-                    elif x[2] == 'ada':
-                        return 1
-                    elif x[2] == 'red':
-                        return 2
+                    return 7
                 elif x[3] == 1982:
-                    if x[1] == 'xbase':
-                        return 3
-                    elif x[1] == 'gn':
-                        return 4
-            elif x[0] == 1960:
-                if x[1] == 'gn':
-                    if x[3] == 2016:
-                        return 7
-                    elif x[3] == 1982:
-                        return 8
-                elif x[1] == 'xbase':
-                    return 6
-            elif x[0] == 1980:
-                return 5
-
-
+                    return 8
+            elif x[1] == 'xbase':
+                return 6
+        elif x[0] == 1980:
+            return 5
