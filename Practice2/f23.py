9b7e236f31fbafc3a7ba368ff7c51059e5ab2ebc
Andrew <frayz2507@gmail.com>
2021-03-14 00:59:26 +0300

    Вариант 11 Задания 2.1, 2.2 и наконец 2.3

--- Practice2/f23.py
+++ Practice2/f23.py
@@ -1,5 +1,5 @@
 def f23(x):
-    preTable = [[f"{round((int(i[0].split()[0][:2])) / 100, 1)}",
+    preTable = [[f"{(round((int(i[0].split()[0][:-1])) / 100, 1))}",
                  i[1].split()[1:],
                  f"{'%s%s' % (i[4][0:6], i[4][8:10])}",
                  f"{i[5][0:i[5].index('@')]}"] for i in x]
@@ -14,6 +14,6 @@ def f23(x):
 #    ['97%', 'Егор Бичберг', None, None, '26.02.2001', 'bicberg89@yandex.ru'],
 #    ['26%', 'Амир Говоший', None, None, '22.03.2004', 'govosij87@yandex.ru'],
 #    ['26%', 'Амир Говоший', None, None, '22.03.2004', 'govosij87@yandex.ru'],
-#   ['89%', 'Артур Ребичак', None, None, '18.04.2002', 'rebicak3@rambler.ru'],
-#    ['92%', 'Мирон Зигий', None, None, '25.06.1999', 'miron28@mail.ru']
+#    ['89%', 'Артур Ребичак', None, None, '18.04.2002', 'rebicak3@rambler.ru'],
+#    ['7%', 'Мирон Зигий', None, None, '25.06.1999', 'miron28@mail.ru']
 # ]))
