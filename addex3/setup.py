7c49606fdc18a80cb6d617e4f8107e134a8f6461
Andrew <frayz2507@gmail.com>
2021-05-30 15:40:21 +0300

    look

---
+++ addex3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="packwithdata",
+    version="1.0.0",
+    author="Andrey",
+    author_email="ilin.a.y1@edu.mirea.ru",
+    packages=["packwithdata"],
+    description="This is a test package for python practice"
+)
